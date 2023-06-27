84c51d73ff3db98d4fd257e3d4c2487e9cb64c8d
Blackie01 <onioludamola@gmail.com>
2023-06-27 18:13:34 +0100

    l

--- src/components/InputShortener.js
+++ src/components/InputShortener.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
+import "./Home.css";
+
 
 const InputShortener = ({ setInputValue }) => {
   const [value, setValue] = useState("");
@@ -6,50 +8,51 @@ const InputShortener = ({ setInputValue }) => {
   const handleClick = () => {
     setInputValue(value);
     setValue("");
-  }
+  };
 
   return (
     <div className="inputContainer">
-      <div 
-      style={{
-        display: 'flex',
-        marginTop: '3rem',
-        justifyContent: 'center'
-      }}
+      <div
+      className="type-link-section"
+        style={{
+          display: "flex",
+          marginTop: "3rem",
+          justifyContent: "center",
+        }}
       >
         <input
-        style={{
-          width: '30%',
-          height: '3rem',
-          borderRadius: '25px',
-          border: '1px solid #0065fe',
-          padding: '0 2rem'
-          
-        }}
+          style={{
+            width: "30%",
+            height: "3rem",
+            borderRadius: "25px",
+            border: "1px solid #0065fe",
+            padding: "0 2rem",
+          }}
           type="text"
           placeholder="Paste a link to shorten it"
           value={value}
-          onChange={e => setValue(e.target.value)}  
+          onChange={(e) => setValue(e.target.value)}
         />
-        <button 
-        style={{
-          marginLeft: '1.5rem',
-          padding: '0 2rem',
-          borderRadius: '25px',
-          border: 'none',
-          backgroundColor: '#0065fe',
-          color: '#fff',
-          cursor: 'pointer',
-          hover: {scale:'1.2'}
-        }}
-        type= 'button' 
-        onClick={handleClick}
+        <button
+        className="type-link-button"
+          style={{
+            marginLeft: "1.5rem",
+            padding: "0 2rem",
+            borderRadius: "25px",
+            border: "none",
+            backgroundColor: "#0065fe",
+            color: "#fff",
+            cursor: "pointer",
+            hover: { scale: "1.2" },
+          }}
+          type="button"
+          onClick={handleClick}
         >
           Shorten
         </button>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default InputShortener;
